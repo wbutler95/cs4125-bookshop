3c65dfbbf1acd39f9c417329f3ab3fc6563ef61d
Jonathan Singer <14136988@studentmail.ul.ie>
2016-10-15 22:46:10 +0100

    Moved Source Files
    Moved William's BookMenu.java, MainMenu.java, StaffMenu.java,
    StockMenu.java, and ThreadedCurrentTime.java to
    CS4125BookShop\src\cs4125bookshop\ where the rest of the source files
    are.

---
+++ CS4125BookShop/src/cs4125bookshop/BookMenu.java
@@ -0,0 +1,199 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.SwingConstants;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BookMenu extends JFrame implements ActionListener
+{
+	private JButton jbtBack;
+	private JButton jbtAddBook;
+	private JButton jbtRemoveBook;
+	private JButton jbtEditBook;
+    private JPanel menu;
+	private JLabel jlblTime;
+	private JLabel jlblDate;
+	private JPanel jpnlTime;
+	private JLabel jlblDate2;
+	private JPanel TimeDate;
+	private String Date;
+	
+	public BookMenu ()
+	{
+		this.setTitle("Book Menu");
+        this.setBounds(100,100,500,300);
+        this.setPreferredSize(new Dimension(500,500));
+        this.setLayout(new GridLayout(1,1));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		TimeDate = new JPanel();
+        TimeDate.setBounds(new Rectangle(50,50));
+		TimeDate.setSize(40, 40);
+        TimeDate.setLayout(new GridLayout(1,4));
+		TimeDate.setBackground(new Color(158, 36, 36));
+        TimeDate.setForeground(Color.WHITE);
+		
+        menu = new JPanel();
+        menu.setBounds(new Rectangle(100,100));
+        menu.setLayout(new GridLayout(5,1));
+		
+        jlblTime = new JLabel("Current Time: ", SwingConstants.CENTER);
+        //jlblTime.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        jlblTime.setSize(40, 40);
+		jlblTime.setBackground(new Color(158, 36, 36));
+        jlblTime.setForeground(Color.WHITE);
+		
+		jlblDate = new JLabel("Current Date: ", SwingConstants.CENTER);
+        //jlblDate.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        jlblDate.setSize(40, 40);
+		jlblDate.setBackground(new Color(158, 36, 36));
+        jlblDate.setForeground(Color.WHITE);
+		
+		Date = new SimpleDateFormat("yyyy/MM/dd").format(new Date());
+		jlblDate2 = new JLabel(Date, SwingConstants.CENTER);
+        //jlblDate2.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        jlblDate2.setSize(40, 40);
+		jlblDate2.setBackground(new Color(158, 36, 36));
+        jlblDate2.setForeground(Color.WHITE);
+        
+        int sleepinterval = 1;
+        ThreadedCurrentTime tc = new ThreadedCurrentTime(sleepinterval);
+        tc.start();
+        jpnlTime = tc.getPanelT();
+        jpnlTime.setBackground(new Color(158, 36, 36));
+		jpnlTime.setForeground(Color.WHITE);
+
+		
+		jbtBack = new JButton("Back");
+        jbtBack.setPreferredSize(new Dimension(100, 100));
+        jbtBack.addActionListener(this);
+		jbtBack.setBackground(new Color(158, 36, 36));
+        jbtBack.setForeground(Color.WHITE);
+		jbtBack.setToolTipText("Click to go back to main menu");
+		
+		jbtAddBook = new JButton("Add Book");
+        jbtAddBook.setPreferredSize(new Dimension(100, 100));
+        jbtAddBook.addActionListener(this);
+		jbtAddBook.setBackground(new Color(158, 36, 36));
+        jbtAddBook.setForeground(Color.WHITE);
+		jbtAddBook.setToolTipText("Click to open add book menu");
+		
+		jbtRemoveBook = new JButton("Remove Book");
+        jbtRemoveBook.setPreferredSize(new Dimension(100, 100));
+        jbtRemoveBook.addActionListener(this);
+		jbtRemoveBook.setBackground(new Color(158, 36, 36));
+        jbtRemoveBook.setForeground(Color.WHITE);
+		jbtRemoveBook.setToolTipText("Click to open remove book menu");
+		
+		jbtEditBook = new JButton("Edit Book");
+        jbtEditBook.setPreferredSize(new Dimension(100, 100));
+        jbtEditBook.addActionListener(this);
+		jbtEditBook.setBackground(new Color(158, 36, 36));
+        jbtEditBook.setForeground(Color.WHITE);
+		jbtEditBook.setToolTipText("Click to open edit book menu");
+		
+		TimeDate.add(jlblTime);
+		TimeDate.add(jpnlTime);
+		TimeDate.add(jlblDate);
+		TimeDate.add(jlblDate2);
+		
+		menu.add(TimeDate);
+		menu.add(jbtAddBook);
+		menu.add(jbtRemoveBook);
+		menu.add(jbtEditBook);
+		menu.add(jbtBack);
+
+		jbtBack.addMouseListener(new java.awt.event.MouseAdapter()
+		{
+			public void mouseEntered(java.awt.event.MouseEvent evt)
+			{
+				jbtBack.setBackground(Color.BLACK);
+			}
+			public void mouseExited(java.awt.event.MouseEvent evt) 
+			{
+				jbtBack.setBackground(new Color(158, 36, 36));
+			}
+		}
+		);
+		
+		jbtAddBook.addMouseListener(new java.awt.event.MouseAdapter()
+		{
+			public void mouseEntered(java.awt.event.MouseEvent evt)
+			{
+				jbtAddBook.setBackground(Color.BLACK);
+			}
+			public void mouseExited(java.awt.event.MouseEvent evt) 
+			{
+				jbtAddBook.setBackground(new Color(158, 36, 36));
+			}
+		}
+		);
+		
+		jbtRemoveBook.addMouseListener(new java.awt.event.MouseAdapter()
+		{
+			public void mouseEntered(java.awt.event.MouseEvent evt)
+			{
+				jbtRemoveBook.setBackground(Color.BLACK);
+			}
+			public void mouseExited(java.awt.event.MouseEvent evt) 
+			{
+				jbtRemoveBook.setBackground(new Color(158, 36, 36));
+			}
+		}
+		);
+
+		jbtEditBook.addMouseListener(new java.awt.event.MouseAdapter()
+		{
+			public void mouseEntered(java.awt.event.MouseEvent evt)
+			{
+				jbtEditBook.setBackground(Color.BLACK);
+			}
+			public void mouseExited(java.awt.event.MouseEvent evt) 
+			{
+				jbtEditBook.setBackground(new Color(158, 36, 36));
+			}
+		}
+		);
+		
+        this.add(menu);
+        this.pack();
+        this.setVisible(true);
+	}
+	
+	public void actionPerformed(ActionEvent ae) 
+	{
+        JButton source = (JButton) ae.getSource();
+        if(source.equals(jbtBack)) {
+            MainMenu menu = new MainMenu();
+			this.setVisible(false);
+        }
+
+        if(source.equals(jbtAddBook)) {
+            //ManageBooksMenu();
+        }
+		
+		if(source.equals(jbtRemoveBook)) {
+            //ManageStockMenu();
+        }
+		
+		if(source.equals(jbtEditBook)) {
+            //ManageStaffMenu();
+        }
+	}
+
+	public static void main(String args [])
+	{
+		BookMenu bmenu = new BookMenu();
+	}
+}
---
+++ CS4125BookShop/src/cs4125bookshop/MainMenu.java
@@ -0,0 +1,207 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.SwingConstants;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainMenu extends JFrame implements ActionListener
+{
+	private JButton jbtExit;
+	private JButton jbtManageBooks;
+	private JButton jbtManageStock;
+	private JButton jbtManageStaff;
+	private JLabel jlblTime;
+	private JLabel jlblDate;
+	private JPanel jpnlTime;
+	private JLabel jlblDate2;
+    private JPanel menu;
+	private JPanel TimeDate;
+	private String Date;
+	
+	public MainMenu ()
+	{
+		this.setTitle("Store Menu");
+        this.setBounds(100,100,500,300);
+        this.setPreferredSize(new Dimension(500,500));
+        this.setLayout(new GridLayout(1,1));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		TimeDate = new JPanel();
+        TimeDate.setBounds(new Rectangle(50,50));
+		TimeDate.setSize(40, 40);
+        TimeDate.setLayout(new GridLayout(1,4));
+		TimeDate.setBackground(new Color(59, 89, 182));
+        TimeDate.setForeground(Color.WHITE);
+		
+        menu = new JPanel();
+        menu.setBounds(new Rectangle(100,100));
+        menu.setLayout(new GridLayout(5,1));
+		
+        jlblTime = new JLabel("Current Time: ", SwingConstants.CENTER);
+        //jlblTime.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        jlblTime.setSize(40, 40);
+		jlblTime.setBackground(new Color(59, 89, 182));
+        jlblTime.setForeground(Color.WHITE);
+		
+		jlblDate = new JLabel("Current Date: ", SwingConstants.CENTER);
+        //jlblDate.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        jlblDate.setSize(40, 40);
+		jlblDate.setBackground(new Color(59, 89, 182));
+        jlblDate.setForeground(Color.WHITE);
+		
+		Date = new SimpleDateFormat("yyyy/MM/dd").format(new Date());
+		jlblDate2 = new JLabel(Date, SwingConstants.CENTER);
+        //jlblDate2.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        jlblDate2.setSize(40, 40);
+		jlblDate2.setBackground(new Color(59, 89, 182));
+        jlblDate2.setForeground(Color.WHITE);
+        
+        int sleepinterval = 1;
+        ThreadedCurrentTime tc = new ThreadedCurrentTime(sleepinterval);
+        tc.start();
+        jpnlTime = tc.getPanelT();
+        jpnlTime.setBackground(new Color(59, 89, 182));
+		jpnlTime.setForeground(Color.WHITE);
+		
+		jbtExit = new JButton("Exit");
+        jbtExit.setPreferredSize(new Dimension(100, 100));
+		//jbtExit.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        jbtExit.addActionListener(this);
+		jbtExit.setBackground(new Color(59, 89, 182));
+        jbtExit.setForeground(Color.WHITE);
+		jbtExit.setToolTipText("Click to Exit");
+		
+		jbtManageBooks = new JButton("Manage Books");
+        jbtManageBooks.setPreferredSize(new Dimension(100, 100));
+		//jbtManageBooks.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        jbtManageBooks.addActionListener(this);
+		jbtManageBooks.setBackground(new Color(59, 89, 182));
+        jbtManageBooks.setForeground(Color.WHITE);
+		jbtManageBooks.setToolTipText("Click to open manage book menu");
+		
+		jbtManageStock = new JButton("Manage Stock");
+        jbtManageStock.setPreferredSize(new Dimension(100, 100));
+		//jbtManageStock.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        jbtManageStock.addActionListener(this);
+		jbtManageStock.setBackground(new Color(59, 89, 182));
+        jbtManageStock.setForeground(Color.WHITE);
+		jbtManageStock.setToolTipText("Click to open manage stock menu");
+		
+		jbtManageStaff = new JButton("Manage Staff");
+        jbtManageStaff.setPreferredSize(new Dimension(100, 100));
+		//jbtManageStaff.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        jbtManageStaff.addActionListener(this);
+		jbtManageStaff.setBackground(new Color(59, 89, 182));
+        jbtManageStaff.setForeground(Color.WHITE);
+		jbtManageStaff.setToolTipText("Click to open manage staff menu");
+		
+		TimeDate.add(jlblTime);
+		TimeDate.add(jpnlTime);
+		TimeDate.add(jlblDate);
+		TimeDate.add(jlblDate2);
+		
+		menu.add(TimeDate);
+		menu.add(jbtManageBooks);
+		menu.add(jbtManageStock);
+		menu.add(jbtManageStaff);
+		menu.add(jbtExit);
+
+		jbtExit.addMouseListener(new java.awt.event.MouseAdapter()
+		{
+			public void mouseEntered(java.awt.event.MouseEvent evt)
+			{
+				jbtExit.setBackground(Color.BLACK);
+			}
+			public void mouseExited(java.awt.event.MouseEvent evt) 
+			{
+				jbtExit.setBackground(new Color(59, 89, 182));
+			}
+		}
+		);
+		
+		jbtManageBooks.addMouseListener(new java.awt.event.MouseAdapter()
+		{
+			public void mouseEntered(java.awt.event.MouseEvent evt)
+			{
+				jbtManageBooks.setBackground(Color.BLACK);
+			}
+			public void mouseExited(java.awt.event.MouseEvent evt) 
+			{
+				jbtManageBooks.setBackground(new Color(59, 89, 182));
+			}
+		}
+		);
+		
+		jbtManageStock.addMouseListener(new java.awt.event.MouseAdapter()
+		{
+			public void mouseEntered(java.awt.event.MouseEvent evt)
+			{
+				jbtManageStock.setBackground(Color.BLACK);
+			}
+			public void mouseExited(java.awt.event.MouseEvent evt) 
+			{
+				jbtManageStock.setBackground(new Color(59, 89, 182));
+			}
+		}
+		);
+
+		jbtManageStaff.addMouseListener(new java.awt.event.MouseAdapter()
+		{
+			public void mouseEntered(java.awt.event.MouseEvent evt)
+			{
+				jbtManageStaff.setBackground(Color.BLACK);
+			}
+			public void mouseExited(java.awt.event.MouseEvent evt) 
+			{
+				jbtManageStaff.setBackground(new Color(59, 89, 182));
+			}
+		}
+		);
+		
+		this.setBackground(new Color(59, 89, 182));
+        this.setForeground(Color.WHITE);
+        this.add(menu);
+        this.pack();
+        this.setVisible(true);
+	}
+	
+	public void actionPerformed(ActionEvent ae) 
+	{
+        JButton source = (JButton) ae.getSource();
+        if(source.equals(jbtExit)) {
+            System.exit(0);
+        }
+
+        if(source.equals(jbtManageBooks)) {
+            BookMenu bmenu = new BookMenu();
+			this.setVisible(false);
+        }
+		
+		if(source.equals(jbtManageStock)) {
+            StockMenu smenu = new StockMenu();
+			this.setVisible(false);
+        }
+		
+		if(source.equals(jbtManageStaff)) {
+            StaffMenu pmenu = new StaffMenu();
+			this.setVisible(false);
+        }
+	}
+
+	public static void main(String args [])
+	{
+		MainMenu menu = new MainMenu();
+	}
+}
